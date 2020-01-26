dafd08991ad5c4338b8dab0d5c10aea011d46958
grgdhiraj <grgdhiraj.be@gmail.com>
2020-01-26 18:25:24 +0100

    Operators

---
+++ script3.js
@@ -0,0 +1,33 @@
+/**Basic Operators**/
+
+var yearJohn = 2020 - 24;
+console.log(yearJohn);
+// Output: John's year of birth
+
+var year, yearJohn, yearMark;
+now = 2020
+yearJohn = now - 24; 
+yearMark = now - 34;
+console.log(yearMark);
+console.log (yearJohn);
+
+// Math operators
+console.log(now+2);
+console.log(now*2);
+console.log(now-35);
+console.log(now/4); 
+
+
+//Logical operators
+var ageJohn, ageMark;
+now = 2020;
+ageJohn = now - 1996;
+ageMark = now - 1986;
+console.log(ageJohn);
+console.log(ageMark);
+
+var oldJohn = ageJohn>ageMark;
+console.log(oldJohn);
+
+var oldMark = ageMark>ageJohn;
+console.log(oldMark);
